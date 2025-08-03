573bf628ece8705c66d56b0dd86d3c80bcf7fbd7
Brittany Ho <t_ho20@u.pacific.edu>
2025-08-03 11:05:11 -0700

    HW25 - Anh Thu

---
+++ homework25/homeworkBC09servlet/src/main/java/homeworkBC09servlet/controller/LoginController.java
@@ -0,0 +1,31 @@
+package homeworkBC09servlet.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "loginController", urlPatterns = {"/login"})
+public class LoginController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("login.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("input_name");
+		String password = req.getParameter("input_password");
+		if (name.equals("admin") && password.equals("123456")) {
+			req.getRequestDispatcher("homepage.jsp").forward(req, resp);
+		} else {
+			req.getRequestDispatcher("login.jsp").forward(req, resp);
+		}
+		
+	}
+
+}
